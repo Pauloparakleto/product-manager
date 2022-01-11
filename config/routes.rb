f483e0d837d19b3fc4891584835b500e5157465a
Felipe <alparakleto@gmail.com>
2022-01-10 23:59:19 -0300

    feat(routes.rb): add post and delete members to resources products

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
-  resources :products, defaults: { format: :json }
+  resources :products, defaults: { format: :json } do
+    member do
+      post :related_products
+      delete "related_products", to: "products#remove_related"
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
