6f9784a99b83f7864d5c37f8b13aaf1ac6d211d0
Felipe <alparakleto@gmail.com>
2022-01-13 13:45:07 -0300

    refactor(application_controller.rb): constants better names

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
 class ApplicationController < ActionController::API
+    ADJUST_OFFSET_TO_ACT_AS_BOOK_PAGE = 1
+    LIMIT_DEFAULT = 20
+    FIRST_PAGE_DEFAULT = 1
+
     def sort_by_param
         set_sort_by_params
     end
@@ -18,13 +22,13 @@ class ApplicationController < ActionController::API
     end
 
     def set_page_params
-        page = params.fetch("page", 1).to_i - 1
+        page = params.fetch("page", FIRST_PAGE_DEFAULT).to_i - ADJUST_OFFSET_TO_ACT_AS_BOOK_PAGE
         page * set_minor_limit
     end
 
     def set_minor_limit
         [
-          params.fetch("limit", 20).to_i,
+          params.fetch("limit", LIMIT_DEFAULT).to_i,
           100
         ].min
     end
