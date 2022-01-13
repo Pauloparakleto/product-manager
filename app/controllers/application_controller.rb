9bc1ad5850d5413713f364edc8fe963f864d060e
Felipe <alparakleto@gmail.com>
2022-01-13 20:22:44 -0300

    refactor(application_controller.rb): sort by column name and sort option, better name to max limit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::API
     ADJUST_OFFSET_TO_ACT_AS_BOOK_PAGE = 1
     LIMIT_DEFAULT = 20
     FIRST_PAGE_DEFAULT = 1
+    MAX_LIMIT_PER_PAGE = 100
 
     def sort_by_param
         set_sort_by_params
@@ -18,7 +19,9 @@ class ApplicationController < ActionController::API
     private
 
     def set_sort_by_params
-        params.fetch("sort_by", "id").to_sym
+        sort_by_column_name = params.fetch("sort_by", "id")
+        sort_option = params.fetch("sort_option", "asc").to_sym
+        { "#{sort_by_column_name}": sort_option}
     end
 
     def set_page_params
@@ -29,7 +32,7 @@ class ApplicationController < ActionController::API
     def set_minor_limit
         [
           params.fetch("limit", LIMIT_DEFAULT).to_i,
-          100
+          MAX_LIMIT_PER_PAGE
         ].min
     end
 end
