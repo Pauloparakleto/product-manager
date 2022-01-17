0ec5415796ce607271da1e5ccd955f94200a1f15
Felipe <alparakleto@gmail.com>
2022-01-16 21:03:01 -0300

    feat(application_controller.rb): set header with pagination count

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,7 +16,20 @@ class ApplicationController < ActionController::API
         set_minor_limit
     end
 
+    def set_header_with_pagination
+        return response.set_header("Count-Pages-Total", 1) if count_queries <= params_limit
+    
+        count_pages = (count_queries / params_limit)
+        remaining_page = count_pages%params_limit
+        count_pages += 1 unless remaining_page.zero?
+        response.set_header("Count-Pages-Total", count_pages)
+      end
+
     private
+
+    def count_queries
+        @query.result.count
+    end
 
     def set_sort_by_params
         sort_by_column_name = params.fetch("sort_by", "id")
